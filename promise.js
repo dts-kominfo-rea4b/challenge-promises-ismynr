61db2b78518d65182ca6178a2f251fd25cacc7a1
ismynr <gekkai7is.my@gmail.com>
2022-09-16 22:46:18 +0700

    promise receiver

--- promise.js
+++ promise.js
@@ -1,7 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotions) => {
+  let promiseTheaterIXXArr = await promiseTheaterIXX();
+  let promiseTheaterVGCArr = await promiseTheaterVGC();
+  let allArr = promiseTheaterIXXArr.concat(promiseTheaterVGCArr);
+  let angry = notAngry = 0;
+
+  allArr.forEach(element => {
+    if (element.hasil === 'marah') angry++;
+    else notAngry++;
+  });
+
+  return emotions === 'marah'
+        ? angry
+        : notAngry;
+};
 
 module.exports = {
   promiseOutput,
